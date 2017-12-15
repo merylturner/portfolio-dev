a80a7136d82dae1a8712f858bae5d33106cd6164
Meryl Turner <merylturner08@gmail.com>
2017-12-14 20:06:59 -0800

    added images

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import github from '../images/git.png';
-
+import styled from 'styled-components';
 // import { Home } from './Components/Home';
 // import { About } from './Components/About';
 // import { Contact } from './Components/Contact';
 
 import '../App.css';
 
+const Social = styled.div`
+    width: 10px;
+    text-align: center;
+`;
+
 function Footer(){
     return(
         <div>
             {/* Social Media Profiles: Linkedin, Github, Twitter, Instagram,  */}
+            <img src={github} alt="github-logo"/>
         </div>
     );
 }
@@ -19,13 +25,12 @@ function Footer(){
 class App extends Component {
     render() {
         return (
-            <div>
-                <div className="App">
+            <div className="App">
+                <div>
                     <header className="App-header">
                         <h1>Hey, I'm Meryl</h1>
                     </header>
-                    <img src={github} alt="github-logo"/>
-                    <p className="App-intro">Software Developer & Graphic Designer</p>
+                    <p className="App-intro">I'm a Software Developer & Graphic Designer born and raised in Portland, Oregon.</p>
                 </div>
                 <div>
                     <Router>
@@ -36,7 +41,7 @@ class App extends Component {
                         </div>
                     </Router>
                 </div>
-                <Footer/>
+                <Social><Footer/></Social>
             </div>
         );
     }
